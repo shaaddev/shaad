628396b08b35bd35a0b5dfdaf1be3a961958d79d
Rashaad Lee Hue <rashaadleehue@qwe.local>
2022-06-03 13:51:45 -0400

    edit

--- src/components/About.js
+++ src/components/About.js
@@ -34,7 +34,7 @@ class About extends React.Component {
                         I am currently studying <strong>Information Systems Management </strong> at <strong> UWI School Of Business & Applied Studies Ltd.</strong> 
                         <br/><br/><br/>
                         <strong>Skills</strong>: <br />
-                        <strong>Python</strong> | <strong>Java</strong> | <strong>JavaScript ES6+</strong> | <strong>React.js</strong> |  <strong>HTML x CSS</strong> |
+                        <strong>Python</strong> | <strong>Java</strong> | <strong>JavaScript ES6+</strong> | <strong>C++</strong> | <strong>React.js</strong> |  <strong>HTML x CSS</strong> |
                         <strong>Git</strong>
                         <br/>
                     </p>
