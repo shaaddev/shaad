c8d27bd58d6b129362de4d0548ac0ad758674569
Rashaad Lee Hue <rashaadleehue@qwe.local>
2022-07-12 12:53:28 -0400

    major changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ class Home extends React.Component {
                     <Link to="/"><h2>Home</h2></Link>
                     <Link to="/about"><h2>About</h2></Link>
                     <Link to="/experience"><h2>Experience</h2></Link>
-                    <Link to="/projects"><h2>Projects</h2></Link>s
+                    <Link to="/projects"><h2>Projects</h2></Link>
                 </div>
 
                 <span className="openbtn" onClick={openNav}>&#9776;</span>
