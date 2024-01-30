932fccbc870a589d55a8d296c7223de94b28c3a8
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-01-30 02:50:30 -0500

    profile update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,7 +6,7 @@ export default function Profile(){
     return (
         <main className="flex flex-col items-center justify-between p-10 md:p-10">
             <div className="flex flex-col md:grid gap-10 md:grid-cols-2 text-neutral-200 md:items-center">
-                <Card className="grayscale hover:filter-none hover:cursor-pointer w-auto h-[400px] rounded-2xl bg-[url('/images/me/444.jpg')] bg-cover bg-center border-none" />
+                <Card className="grayscale hover:filter-none hover:cursor-pointer w-auto h-[400px] rounded-2xl bg-[url('/images/me/me.jpg')] bg-cover bg-center border-none" />
 
                 <div className="text-center md:text-left items-center">
                     <h1 className="text-2xl font-bold">Shaad Lee Hue</h1>
