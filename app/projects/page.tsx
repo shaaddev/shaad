93bc0093c2143e49baa3143d41042b44196eb669
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-02-15 01:47:13 -0500

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,7 +17,7 @@ export default function Projects(){
             <div className="flex flex-col justify-center lg:grid lg:grid-cols-3 lg:gap-4 lg:p-10 xl:p-24">
                 {projects.map((p: any) => (
                     <div key={p.title} className="flex flex-col items-center justify-center">
-                        <Card  className="w-full h-auto lg:w-full lg:h-full text-black bg-neutral-50 shadow shadow-black/10 dark:bg-zinc-800 dark:text-neutral-200 mb-1 border-none rounded-2xl p-2">
+                        <Card  className="w-full h-auto lg:w-full lg:h-full mb-5 md:mb-0 text-black bg-neutral-50 shadow shadow-black/10 dark:bg-zinc-800 dark:text-neutral-200 border-none rounded-2xl p-2">
                             <CardHeader>
                                 <CardDescription className="inline-flex text-black dark:text-slate-200 gap-2 text-xl">
                                     <Link href={p.link} target="_blank" className="hover:opacity-75"><FaGithub /></Link>
