db10275eeb866f70391e5f0852e44de24c237c80
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-02-15 16:36:30 -0500

    responsive update

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -1,6 +1,6 @@
 export default function Copyright(){
     return(
-        <div className="fixed bottom-2 left-5 -z-10">
+        <div className="fixed bottom-2 left-5 -z-10 hidden lg:block">
             <p className="text-sm text-slate-800 dark:text-slate-300">&copy; {new Date().getFullYear()} Made by <span className="text-[#009be6]">S.L.H</span></p>
         </div>
     )
