8dee634a2d8a4a6d9a3febee39b9c4250b045577
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-02-15 16:08:53 -0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Theme from "@/components/Theme";
+import Copyright from "@/components/Copyright";
 
 const mont = Montserrat({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         
         <main>{children}</main>
         <Theme />
+        <Copyright />
       </body>
     </html>
   );
