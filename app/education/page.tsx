1004c5c150313d9dafd5364ceeb27ad26929d2c6
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-02-27 06:09:00 -0500

    fixed card on edu

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -7,11 +7,11 @@ import { education } from "@/constants/info"
 export default function Education(){
     
     return(
-        <main className="flex flex-col items-center justify-between p-10 ">
+        <main className="flex flex-col items-center justify-between p-10 xl:mx-72 2xl:mx-96">
             <h1 className="font-semibold text-black dark:text-zinc-200 text-2xl mb-5">EDUCATION</h1>
             {education.map((e: any) => (
                 <div key={e.id} className="flex flex-col items-center justify-center">
-                    <Card className="w-full md:w-4/5 lg:w-3/5 2xl:w-2/5 h-auto text-black dark:text-zinc-200 bg-neutral-50 shadow shadow-black/10 dark:bg-zinc-800 border-none mb-10 rounded-2xl p-3">
+                    <Card className="w-full h-auto text-black dark:text-zinc-200 bg-neutral-50 shadow shadow-black/10 dark:bg-zinc-800 border-none mb-10 rounded-2xl p-3">
                         <CardContent>
                             <h4 className="mt-5 text-base lg:text-xl font-semibold">{e.title + " @"} <span className="text-rose-700">{e.school}</span></h4> 
                             <p className="mt-5 text-sm font-semibold">{e.degree}</p>
