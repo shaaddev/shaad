33c935b092db6d33dd4b4c390a057ba704af0fd4
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-10-16 04:00:21 -0400

    cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,17 @@ import createMDX from "@next/mdx";
 import rehypePrettyCode from "rehype-pretty-code";
 
 const nextConfig = {
-  pageExtensions: ['mdx', 'ts', 'tsx'],
+  pageExtensions: ["mdx", "ts", "tsx"],
   experimental: {
-    mdxRs: false,
-  }
+    mdxRs: true,
+  },
 };
 
 const withMDX = createMDX({
   options: {
     remarkPlugins: [],
     rehypePlugins: [[rehypePrettyCode, { theme: "github-dark-high-contrast" }]],
-  }
+  },
 });
 
 export default withMDX(nextConfig);
