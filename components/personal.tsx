31e2b97b64ed6a4a54c2b21c365455a5a587de21
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-10-17 04:57:01 -0400

    fix: ui consistency

--- components/personal.tsx
+++ components/personal.tsx
@@ -4,23 +4,28 @@ import { Link } from "next-view-transitions";
 
 export function Personal() {
   const route = [
-    { name: 'Contact Me', url: '/say_hi'},
-    { name: 'Resume', url: '/CV.pdf'},
-    { name: 'Article', url: '/w/ai-sdk-core'},
-  ]
+    { name: "Contact Me", url: "/say_hi" },
+    { name: "Resume", url: "/CV.pdf" },
+    { name: "Article", url: "/w/ai-sdk-core" },
+  ];
 
   return (
-    <div className="flex flex-row gap-2 my-5">
-      {route.map((r) => (
-        <Link
-          key={r.name}
-          href={r.url}
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: "outline", size: "sm", }))}
-        >
-          {r.name}
-        </Link>
-      ))}
-    </div>
-  )
+    <section id="personal">
+      <div className="flex min-h-0 flex-col gap-y-3 my-12">
+        <h2 className="text-xl font-bold">Personal</h2>
+        <div className="flex flex-row gap-2">
+          {route.map((r) => (
+            <Link
+              key={r.name}
+              href={r.url}
+              rel="noopener noreferrer"
+              className={cn(buttonVariants({ variant: "outline", size: "sm" }))}
+            >
+              {r.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
