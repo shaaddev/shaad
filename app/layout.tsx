6e53e610a09d906ddf17892d44eb169c2cbea11c
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-10-12 19:06:26 -0400

    rm

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { ViewTransitions } from "next-view-transitions";
-import { Copyright } from "@/components/footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -42,7 +41,6 @@ export default function RootLayout({
             <Providers>
               <main className="w-full flex items-center justify-center">
                 {children}
-                <Copyright />
               </main>
             </Providers>
           </div>
