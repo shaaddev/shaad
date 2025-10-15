90e1edad9510cda07fd9cd81a1ccfcda57304209
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-10-14 20:00:34 -0400

    standard

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Masira
+            Masira.
           </Link>
         </span>
       </h1>
