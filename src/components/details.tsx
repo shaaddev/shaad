00ba2c75ad39b6663015a8f539330f72a58e7450
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2025-12-14 22:33:06 -0500

    basic

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -1,6 +1,6 @@
 export function Details() {
   return (
-    <section className="flex flex-col items-center justify-center h-screen bg-neutral-900 text-white">
+    <section className="flex flex-col items-center justify-center h-screen bg-neutral-950 text-white">
       <div className="flex flex-col items-start justify-start">
         <h1>hello, shaad here</h1>
         <p>
